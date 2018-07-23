22332d67334061896929ce4de06d26ed63bb6853
Ayman Boustati <ayman.boustati@outlook.com>
2018-07-23 16:42:10 +0100

    created setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import import setup
+
+setup(name='at_gp',
+            version='0.1dev',
+            author='Ayman Boustati',
+            author_email='aboustati@turing.ac.uk',
+            license='Apache License 2.0',
+            packages=['transfer_gp']
+            #test_suite='tests.test_module_suite'
+     )
